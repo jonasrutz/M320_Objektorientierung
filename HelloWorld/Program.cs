8d0a08c01e0b872355ed241befc74383d8ab9732
jonasrutz <joaarugu@gmail.com>
2025-08-18 09:08:15 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 16;
+            Console.WriteLine("Hello, World!");
+            Console.WriteLine("My name is Jonas");
+            Console.WriteLine($"I'm {a} years old");
+        }
+    }
+}
