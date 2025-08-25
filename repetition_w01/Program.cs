967b238dda47efa3068080eec6de341307af43f0
jonasrutz <joaarugu@gmail.com>
2025-08-25 09:52:49 +0200

    v02

---
+++ repetition_w01/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace repetition_w01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr;
+            arr = new int[5];
+            arr[3] = 200;
+            arr[0] = 450;
+            arr[1] = 700;
+            arr[2] = 175;
+            arr[4] = 250;
+
+            int summe = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                summe += arr[i];
+            }
+
+            Console.WriteLine("Summe:" +summe);
+        }
+    }
+}
